2da072dde68def855fdee6062a704f9bca046d90
Jibon Krishna Joy <47941812+JibonKrishna@users.noreply.github.com>
2020-10-23 16:53:41 +0600

    Add files via upload

---
+++ power&log.py
@@ -0,0 +1,14 @@
+"""
+Created on Friday, October 23, 2020
+
+@author: Jibon_Krishna
+"""
+
+
+import numpy
+
+x = int(input("enter x: "))
+y = int(input("enter y: "))
+
+print(f"x^y= {x**y}")
+print(f"logx(base2)= {numpy.log2(x)}")
