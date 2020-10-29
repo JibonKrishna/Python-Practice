3eaaf40ba590e57d5c2fb5ba1df20fa196e692c6
Jibon Krishna Joy <47941812+JibonKrishna@users.noreply.github.com>
2020-10-29 21:04:37 +0600

    Add files via upload

---
+++ largest_number_in_a_sequence.py
@@ -0,0 +1,34 @@
+"""
+Created on Thursday, October 29, 2020
+
+@author: Jibon_Krishna
+"""
+
+
+def largest(s):
+    """Takes sequence of numbers separated by comma as input and returns
+    the largest number. If the input is "1.2,1,5,2,4,5.2,1,5", it will 
+    return 5.2
+
+    Args:
+        s (str): A sequence of number separated by comma.
+    """
+    x = s.split(",")
+    largest = 0
+    i = 0
+    try:
+        while i+1 < len(x):
+            if largest < float(x[i]) and\
+                    float(x[i]) > float(x[i+1]):
+                largest = float(x[i])
+            elif float(x[i+1]) > largest:
+                largest = float(x[i+1])
+            i += 1
+        return largest
+    except ValueError:
+        print("\n\tWrong input! \n")
+
+
+number = str(input("Enter a sequence of number separated by comma: "))
+if largest(number) != None:
+    print(f"The largest number of the sequence is {largest(number)}.")
