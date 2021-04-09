75b999179fae199af1289d7062ff0b8218904f3e
Jibon Krishna Joy <47941812+JibonKrishna@users.noreply.github.com>
2021-04-09 13:36:10 -0500

    Update vowel_counter.py

--- vowel_counter.py
+++ vowel_counter.py
@@ -5,11 +5,22 @@ Created on Friday, November 27, 2020
 """
 
 # Takes a string as input and prints the number of vowels in the input string.
-string = str(input("Input a string: ").lower())
-count = 0
+
+# converts the input string to a list of unique characters
+string = list(set(input("Enter your string: ").lower()))
+
+
 vowel = ['a', 'e', 'i', 'o', 'u']
+count = 0
+
 
 for letter in string:
     if letter in vowel:
         count += 1
-print(f"There are {count} vowels in your input string")
+
+
+# Singular-plural
+if count > 1:
+    print(f"There are {count} vowels in your input.")
+else:
+    print(f"There is only {count} vowel in your input.")
